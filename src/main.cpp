ba7b4a5dc1eec9e720332eb808cd91c43113525b
Lt-Henry <quiqueiii@gmail.com>
2014-05-26 19:03:30 +0200

    Added Core class

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 
 
 #include <iostream>
+#include "Core.hpp"
+
 
 using namespace std;
+using namespace com::toxiclabs::iris;
 
 
 int main(int argc,char * argv[])
 {
 	cout<<"Iris path-tracer"<<endl;
+	
+	Core core(argc,argv);
+	
+	core.Run();
 	
 	return 1;
 }
