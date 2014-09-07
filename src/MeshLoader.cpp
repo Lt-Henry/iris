78477a2df65e5332ae257c7f3cc2fb462d237c0f
Lt-Henry <quiqueiii@gmail.com>
2014-09-07 13:01:06 +0200

    progress parsing obj files with c++11 regex

--- src/MeshLoader.cpp
+++ src/MeshLoader.cpp
@@ -13,6 +13,17 @@ using namespace std;
 
 void MeshLoader::Load(string filename)
 {
+
+	//regex patterns
+	regex comment("^#(.|\\s)*");
+	regex usemtl("^usemtl\\s+([a-zA-Z]+)");
+	regex mtllib("^mtllib\\s+(.+)");
+	regex o("^o\\s+(.+)");
+	regex v("^v\\s+(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)");
+	regex vn("^vn\\s+(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)");
+	regex vt("^vt\\s+(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)");
+	regex f("^f\\s+(\\d+)/(\\d+|)/(\\d+|)\\s+(\\d+)/(\\d+|)/(\\d+|)\\s+(\\d+)/(\\d+|)/(\\d+|)");
+	
 	ifstream file(filename);
 	
 	while(!file.eof())
@@ -20,7 +31,36 @@ void MeshLoader::Load(string filename)
 		string line;
 		getline(file,line);
 		
-		if(line[0]=='o')cout<<line<<endl;
+		match_results<string::const_iterator> results;
+		
+		if(regex_match(line,comment))
+			cout<<"comment:"<<line<<endl;		
+				
+		if(regex_match(line,results,usemtl))
+		{
+			cout<<"usemtl:"<<results[1]<<endl;
+		}
+		
+		if(regex_match(line,results,mtllib))
+		{
+			cout<<"mtllib:"<<results[1]<<endl;
+		}
+		
+		
+		if(regex_match(line,results,o))
+		{
+			cout<<"Object:"<<results[1]<<endl;
+		}
+		
+		if(regex_match(line,results,f))
+		{
+			cout<<"Face"<<endl;
+			for(int n=1;n<results.size();n++)
+			{
+				cout<<results[n]<<endl;
+			}
+		}
+
 	}
 	
 	file.close();
