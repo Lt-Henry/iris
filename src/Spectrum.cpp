83accf28b6e7181c99bf7a8baf04fc7cc775cbbb
Lt-Henry <quiqueiii@gmail.com>
2014-10-04 20:38:58 +0200

    Added Spectrum files

---
+++ src/Spectrum.cpp
@@ -0,0 +1,10 @@
+#include "Spectrum.hpp"
+
+using namespace std;
+using namespace com::toxiclabs::iris;
+
+
+CoefficientSpectrum::CoefficientSpectrum()
+{
+
+}
---
+++ src/Spectrum.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+#ifndef _IRIS_SPECTRUM_
+#define _IRIS_SPECTRUM_
+
+namespace com
+{
+	namespace toxiclabs
+	{
+		namespace iris
+		{
+			class CoefficientSpectrum
+			{
+				public:
+				
+				CoefficientSpectrum();
+			};
+			
+		}
+	}
+}
+
+
+#endif
