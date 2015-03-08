fc72d0e928738bf932ff322a451e4bae635e96f2
Lt-Henry <quiqueiii@gmail.com>
2015-03-09 00:11:38 +0100

    * Working lua scripts (early work) * No longer using multiple cameras, one is enought * Analyzing stratified distribution data * Added a pair of test scenes

---
+++ scenes/test-02/setup.lua
@@ -0,0 +1,8 @@
+
+
+
+io.write("Test Scene 02\n")
+
+SetScreen(800,600,2)
+
+LoadMesh("test-02.obj")
---
+++ scenes/test-03/setup.lua
@@ -0,0 +1,8 @@
+
+
+
+io.write("Test Scene 03\n")
+
+SetScreen(800,600,2)
+SetCamera({2.0,6.0,-10.0},{0.0,2.0,0.0})
+LoadMesh("test-03.obj")
